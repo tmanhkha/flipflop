5c640ecc9886d1aa84698af84ece8ace3f31499f
Manh Kha <manhkha.thai@gmail.com>
2020-04-29 12:37:16 +0700

    change name migration

--- lib/generators/flipflop/migration/templates/create_features.rb
+++ lib/generators/flipflop/migration/templates/create_features.rb
@@ -1,6 +1,6 @@
-class CreateFeatures < ActiveRecord::Migration<%= Rails.version >= "5" ? "[#{ActiveRecord::Migration.current_version}]" : "" %>
+class CreateFlipFeatures < ActiveRecord::Migration<%= Rails.version >= "5" ? "[#{ActiveRecord::Migration.current_version}]" : "" %>
   def change
-    create_table :flipflop_features do |t|
+    create_table :flip_features do |t|
       t.string :key, null: false
       t.boolean :enabled, null: false, default: false
 
